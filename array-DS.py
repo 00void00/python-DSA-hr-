6fbd057571254f30b3c78144012fadf7602cc091
00void00 <63497724+00void00@users.noreply.github.com>
2021-08-02 12:30:31 +0530

    Create array-DS.py

---
+++ array-DS.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'reverseArray' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def reverseArray(a):
+    # method-1 -> slicing
+      #return a[::-1]
+    # method-2 -> built-in function
+      #a.reverse()
+      #return a
+    #method-3 -> built-in function
+    return [i for i in reversed(a)]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
